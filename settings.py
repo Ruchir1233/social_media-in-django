faca4df84c4481938007967b7d8b7e3afae63f93
Ruchir1233 <90233530+Ruchir1233@users.noreply.github.com>
2022-11-01 14:34:27 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-vtgb%%ph!6f94opo=i-cxjs77b95x63ywwz33*4ovs-s%)92oh'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
